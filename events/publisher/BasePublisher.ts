6aaa9c86bf8afcb44799915a18c9692396275d0f
GreatEki <ekeneidiagbor@gmail.com>
2023-05-05 18:01:04 +0100

    fix: create channel rejig

--- events/publisher/BasePublisher.ts
+++ events/publisher/BasePublisher.ts
@@ -9,23 +9,13 @@ interface Events {
 export abstract class BasePublisher<T extends Events> {
   abstract routingKey: T["routingKey"];
 
-  channel: Channel;
+  protected channel: Channel;
 
   constructor(channel: Channel) {
     this.channel = channel;
   }
 
-  async createChannel() {
-    const connection = await amqp.connect(process.env.RABBITMQ_URL!);
-
-    this.channel = await connection.createChannel();
-  }
-
   async publish(message: T["message"]): Promise<void> {
-    if (!this.channel) {
-      this.createChannel();
-    }
-
     const exchangeName = process.env.EXCHANGE_NAME as string;
 
     await this.channel.assertExchange(exchangeName, "direct");
