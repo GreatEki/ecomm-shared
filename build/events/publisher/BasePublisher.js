0e4d69a3ff5d829774444bb826c27f1c9810aad9
GreatEki <ekeneidiagbor@gmail.com>
2023-05-05 19:10:57 +0100

    feat: rabbitmq base listener

--- build/events/publisher/BasePublisher.js
+++ build/events/publisher/BasePublisher.js
@@ -8,27 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BasePublisher = void 0;
-const amqplib_1 = __importDefault(require("amqplib"));
 class BasePublisher {
     constructor(channel) {
         this.channel = channel;
     }
-    createChannel() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const connection = yield amqplib_1.default.connect(process.env.RABBITMQ_URL);
-            this.channel = yield connection.createChannel();
-        });
-    }
     publish(message) {
         return __awaiter(this, void 0, void 0, function* () {
-            if (!this.channel) {
-                this.createChannel();
-            }
             const exchangeName = process.env.EXCHANGE_NAME;
             yield this.channel.assertExchange(exchangeName, "direct");
             this.channel.publish(exchangeName, this.routingKey, Buffer.from(JSON.stringify(message)));
