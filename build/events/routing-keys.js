644e3ae94ae3c53ef4f3082e13ccfe13bf5f53de
GreatEki <ekeneidiagbor@gmail.com>
2023-05-05 22:24:21 +0100

    chore: removed build folder

--- build/events/routing-keys.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoutingKey = void 0;
-var RoutingKey;
-(function (RoutingKey) {
-    RoutingKey["UserCreated"] = "user:created";
-})(RoutingKey = exports.RoutingKey || (exports.RoutingKey = {}));
