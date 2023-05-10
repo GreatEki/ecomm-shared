bc802f20a0b4b52735eea4017f5f07bcffb35d09
GreatEki <ekeneidiagbor@gmail.com>
2023-05-10 11:12:42 +0100

    feat: impl admin guard

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,5 +1,6 @@
 import { currentUser } from "./current-user";
 import { requireAuth } from "./require-auth";
 import { errorHandler } from "./error-handler";
+import { adminGuard } from "./role-check";
 
-export { currentUser, requireAuth, errorHandler };
+export { currentUser, requireAuth, errorHandler, adminGuard };
