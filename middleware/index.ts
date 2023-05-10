660aeefc7c242201dadbba841a73df16866a1880
GreatEki <ekeneidiagbor@gmail.com>
2023-05-10 12:37:44 +0100

    fix: fixed broken builds

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,6 +1,5 @@
 import { currentUser } from "./current-user";
 import { requireAuth } from "./require-auth";
 import { errorHandler } from "./error-handler";
-import { adminGuard } from "./role-check";
 
-export { currentUser, requireAuth, errorHandler, adminGuard };
+export { currentUser, requireAuth, errorHandler };
